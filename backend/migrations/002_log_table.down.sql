e345e06e2490d37b39920a7de600afbe78ff7880
Taher Alfayad <taheralfayad@gmail.com>
2026-04-23 19:23:35 -0400

    feat(log table): add log table migration

---
+++ backend/migrations/002_log_table.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS request_log_controller_idx;
+DROP INDEX IF EXISTS request_log_created_at_idx;
+DROP INDEX IF EXISTS request_log_ip_address_idx;
+DROP TABLE IF EXISTS request_log;
