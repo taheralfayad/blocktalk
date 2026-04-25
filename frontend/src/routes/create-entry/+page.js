676c9fee1bb5b4b9989522456161e303c1fa4c91
Taher Alfayad <taheralfayad@gmail.com>
2026-04-25 17:24:26 -0400

    refactor(isLoggedIn): refactor isLoggedIn calls on frontend to include verification information

--- frontend/src/routes/create-entry/+page.js
+++ frontend/src/routes/create-entry/+page.js
@@ -2,7 +2,10 @@ import { redirect } from '@sveltejs/kit';
 import { isLoggedIn } from '$lib/utils/utils.svelte.js';
 
 export const load = async () => {
-  if (!(await isLoggedIn())) {
+  
+  const logInData = await isLoggedIn();
+  
+  if (!!(!logInData.userIsLoggedIn || !logInData.userIsVerified)) {
     redirect(302, '/login');
   }
 };
