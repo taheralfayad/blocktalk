6c5f184a8d78edaf098faa2e85e0aadf9c8fb2f6
Taher Alfayad <taheralfayad@gmail.com>
2026-05-01 14:57:04 -0400

    feat(user profile): add layout for user profile

---
+++ frontend/src/lib/design-system/kpi.svelte
@@ -0,0 +1,6 @@
+<div class="border rounded-md items-center justify-center bg-slate-300">
+  <div class="flex flex-col p-24 justify-center items-center">
+    <h2 class="text-lg">67</h2>
+    <p>Cities</p>
+  </div>
+</div>
---
+++ frontend/src/routes/user-profile/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Kpi from "$src/lib/design-system/kpi.svelte";
+</script>
+
+<div class="flex flex-col border min-h-screen justify-center items-center">
+  <div class="flex flex-row gap-2">
+    <Kpi />
+    <Kpi />
+  </div>
+</div>
